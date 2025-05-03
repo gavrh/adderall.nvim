50fa8262136d1f4257741496926c527ebcf1141f
Gavin Holmes <gavin@gav.codes>
2025-05-03 12:36:13 -0700

    mostly configured

---
+++ lua/adderall/colors.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+M.default = {
+    -- basic
+    -- bg = "#191c25",
+    bg = "#ffffff",
+    fg = "#4b4e57",
+    -- normal
+    black = "#32353e",
+    red = "#ce0326",
+    green = "#489577",
+    yellow = "#f7b218",
+    blue = "#3c6274",
+    magenta = "#7a82da",
+    cyan = "#477476",
+    white = "#f3f3f3",
+    -- bright
+    bright_black = "#4b4e57",
+    bright_red = "#f8002d",
+    bright_green = "#00ad82",
+    bright_yellow = "#f9c859",
+    bright_blue = "#2d6c9c",
+    bright_magenta = "#ff78f8",
+    bright_cyan = "#3a8187",
+    bright_white = "#ffffff",
+    -- grays
+    gray01 = "#1d1f28",
+    gray02 = "#232530",
+    gray03 = "#292c37",
+    gray04 = "#363946",
+    gray05 = "#424451",
+    gray06 = "#909090",
+    gray07 = "#e0e0e0",
+    -- special
+    none = "none",
+}
+
+return M
